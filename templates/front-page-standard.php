914a7bcee6d2f165dc18753aba6ca1e424b3892d
Rusty Eddy <rusty@lakeparkonline.com>
2014-10-10 18:22:44 -0700

    Added some of the super hero stuff

--- templates/front-page-standard.php
+++ templates/front-page-standard.php
@@ -3,14 +3,26 @@
 require_once( 'lib/utils.php' );
 require_once( 'lib/super-hero.php' );
 
+add_filter( 'body_class', 'gs_body_class' );
+function gs_body_class( $classes ) {
+	$classes[] = 'front-page';
+	return $classes;
+}
+
 gs_standard_front_page( 'gs_custom_loop' );
 
 function gs_custom_loop()
 {
-	echo "FOO";
-	$html = "<h1>Hello, World!</h1>";
+	gs_super_hero( 'gs_super_hero_overlay' );
+}
 
-	gs_super_hero( $html );
+function gs_super_hero_overlay()
+{
+	?>
+	<div class='super-hero-overlay'>
+		<h1>Hello, World</h1>
+	</div>
+	<?php
 }
 
 genesis();
