4c09fa6c46190536aa76d51e0df301450434e2da
Rusty Eddy <rusty@lakeparkonline.com>
2014-10-21 17:17:30 -0700

    Added the ability to load bootstrap

--- lib/utils.php
+++ lib/utils.php
@@ -53,6 +53,32 @@ function gs_load_google_fonts( $label, $family )
 }
 
 /**
+ * Load Bootstrap in it's entirety
+ */
+function gs_load_bootstrap()
+{
+    function enqueue_bootstrap_css() {
+        wp_enqueue_style( 'bootstrap',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' );
+
+        wp_enqueue_style( 'bootstrap-theme',
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css' );
+    }
+
+    function enqueue_bootstrap_js()
+    {
+        wp_enqueue_script ('bootstrap-js', 
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'
+
+            );
+    }
+
+    //* Make Font Awesome available
+    add_action( 'wp_enqueue_style', 'enqueue_bootstrap_css' );
+    add_action( 'wp_enqueue_scripts', 'enqueue_bootstrap_js' );
+}
+
+/**
  * Move primary navbar above header
  * -----------------------------------------------------------------------------
  */
